09ff33e82f94c278180c1144efc87e6c662b1455
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-11-29 14:14:38 -0300

    Main deletada e WindowScene criada

---
+++ Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Menu
+//
+//  Created by Livia Carvalho Keller on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemPink
+    }
+
+
+}
+
